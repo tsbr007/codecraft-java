4c2a041f26c901a9b8b7a9dd1fd72a9a977b9d0c
Balaji <tsbr007@gmail.com>
2025-08-21 10:43:15 +0530

    SqRoot

---
+++ utils/SQRoot.java
@@ -0,0 +1,29 @@
+package utils;
+
+public class SQRoot {
+
+    private static int getSQRoot(int num) {
+        int start = 0;
+        int end = num;
+        int mid = num / 2;
+
+        while (mid != start) {
+            if (mid * mid == num) {
+                return mid;
+            } else if (mid * mid > num) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+            mid = (end + start) / 2;
+        }
+
+        return num;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getSQRoot(256));
+
+    }
+}
